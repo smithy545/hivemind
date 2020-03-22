59c02601ec3dd2d9f2940e39daeacc43bdc6e324
Philip Smith <smithy545@gmail.com>
2020-03-21 23:52:40 -0400

    Switch to GL_NEAREST for min/mag filters (becomes pixelated upon zoom instead of blurry)

--- src/util/RenderUtil.cpp
+++ src/util/RenderUtil.cpp
@@ -95,8 +95,8 @@ GLuint RenderUtil::loadTexture(const std::string &texturePath, int &width, int &
     // texture parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glGenerateMipmap(GL_TEXTURE_2D);
 
     delete[] data;
