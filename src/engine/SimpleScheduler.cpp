6042fb70a17006a35c0fd6c738561dea0844bdb8
Philip Smith <smithy545@gmail.com>
2020-06-16 16:42:21 -0400

    Add scheduling stubs

---
+++ src/engine/SimpleScheduler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Philip on 6/16/2020.
+//
+
+#include "SimpleScheduler.h"
---
+++ src/engine/SimpleScheduler.h
@@ -0,0 +1,16 @@
+//
+// Created by Philip on 6/16/2020.
+//
+
+#ifndef SOCIETY_SIMPLESCHEDULER_H
+#define SOCIETY_SIMPLESCHEDULER_H
+
+#include "Scheduler.h"
+
+
+class SimpleScheduler : public Scheduler {
+
+};
+
+
+#endif //SOCIETY_SIMPLESCHEDULER_H
